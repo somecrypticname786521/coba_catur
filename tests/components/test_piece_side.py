bd3d3d931251f5305dd9fedea3de86cd44b43aad
Tari <Tari@gmail.com>
2018-08-17 09:28:16 +0700

    Unit test

--- tests/components/test_piece_side.py
+++ tests/components/test_piece_side.py
@@ -3,6 +3,6 @@ import unittest
 
 class TestPieceSide(unittest.TestCase):
     def test_enum_is_right(self):
-        self.assertEqual(PieceSide.WHITE, 1)
-        self.assertEqual(PieceSide.BLACK, -1)
+        self.assertEqual(PieceSide.WHITE.value, 1)
+        self.assertEqual(PieceSide.BLACK.value, -1)
 
